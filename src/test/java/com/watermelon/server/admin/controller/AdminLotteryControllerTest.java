811252c5a97d5c31ffdaf1c74a1cb7d511ae62c4
ysndy <ysndy@naver.com>
2024-08-11 15:06:21 +0900

    refactor[api-server]: 추첨 어드민 컨트롤러 테스트 given, when 절 메소드로 추출하는 방식으로 수정

--- src/test/java/com/watermelon/server/admin/controller/AdminLotteryControllerTest.java
+++ src/test/java/com/watermelon/server/admin/controller/AdminLotteryControllerTest.java
@@ -28,8 +28,8 @@ import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.docume
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AdminLotteryController.class)
 class AdminLotteryControllerTest extends ControllerTest {
@@ -47,28 +47,20 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("응모자 명단을 반환한다.")
     void getLotteryAppliers() throws Exception {
 
-        //given
-        Pageable pageable = PageRequest.of(TEST_PAGE_NUMBER, TEST_PAGE_SIZE);
-        Page<ResponseLotteryApplierDto> expected = new PageImpl<>(List.of(
-                ResponseLotteryApplierDto.createTestDto(),
-                ResponseLotteryApplierDto.createTestDto()
-        ), pageable, TEST_PAGE_SIZE);
+        givenLotteryApplierList();
 
-        Mockito.when(lotteryService.getApplierInfoPage(pageable))
-                .thenReturn(expected);
+        whenLotteryApplierListAreRetrievedForAdmin();
 
-        //when & then
-        this.mockMvc.perform(get(PATH_ADMIN_APPLIER)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
-                        .param(PARAM_PAGE, String.valueOf(TEST_PAGE_NUMBER))
-                        .param(PARAM_SIZE, String.valueOf(TEST_PAGE_SIZE))
-                ).andExpect(content().json(objectMapper.writeValueAsString(expected)))
+        resultActions
+                .andExpect(jsonPath("content[0].uid").isString())
+                .andExpect(jsonPath("content[0].link").isString())
+                .andExpect(jsonPath("content[0].remainChance").isNumber())
+                .andExpect(jsonPath("content[0].rank").isNumber())
+                .andDo(print())
                 .andDo(document(DOCUMENT_NAME_ADMIN_APPLIER,
                         resource(
                                 ResourceSnippetParameters.builder()
                                         .description("응모자 명단 조회")
-                                        .requestHeaders(
-                                                headerWithName(HEADER_NAME_AUTHORIZATION).description("Bearer token for authentication"))
                                         .queryParameters(
                                                 parameterWithName(PARAM_PAGE).description("페이지"),
                                                 parameterWithName(PARAM_SIZE).description("페이지 크기")
@@ -83,19 +75,17 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("추첨 당첨자 명단을 반환한다.")
     void getLotteryWinners() throws Exception {
 
-        //given
-        List<ResponseAdminLotteryWinnerDto> expected = List.of(
-                ResponseAdminLotteryWinnerDto.createTestDto(),
-                ResponseAdminLotteryWinnerDto.createTestDto()
-        );
+        givenLotteryWinners();
 
-        Mockito.when(lotteryWinnerService.getAdminLotteryWinners())
-                .thenReturn(expected);
+        whenLotteryWinnerListAreRetrievedForAdmin();
 
-        //when & then
-        this.mockMvc.perform(get(PATH_ADMIN_LOTTERY_WINNERS)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
-                ).andExpect(content().json(objectMapper.writeValueAsString(expected)))
+        resultActions
+                .andExpect(jsonPath("[0].uid").isString())
+                .andExpect(jsonPath("[0].name").isString())
+                .andExpect(jsonPath("[0].phoneNumber").isString())
+                .andExpect(jsonPath("[0].address").isString())
+                .andExpect(jsonPath("[0].rank").isNumber())
+                .andExpect(jsonPath("[0].status").isString())
                 .andDo(document(DOCUMENT_NAME_LOTTERY_WINNERS, resourceSnippetAuthed("추첨 당첨자 명단 조회")));
 
     }
@@ -104,19 +94,17 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("파츠 당첨자 명단을 반환한다.")
     void getPartsWinners() throws Exception {
 
-        //given
-        List<ResponseAdminPartsWinnerDto> expected = List.of(
-                ResponseAdminPartsWinnerDto.createTestDto(),
-                ResponseAdminPartsWinnerDto.createTestDto()
-        );
+        givenPartsWinnerList();
 
-        Mockito.when(partsService.getAdminPartsWinners())
-                .thenReturn(expected);
+        whenPartsWinnerListAreRetrievedForAdmin();
 
-        //when & then
-        this.mockMvc.perform(get(PATH_ADMIN_PARTS_WINNERS)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
-                ).andExpect(content().json(objectMapper.writeValueAsString(expected)))
+        resultActions
+                .andExpect(jsonPath("[0].uid").isString())
+                .andExpect(jsonPath("[0].name").isString())
+                .andExpect(jsonPath("[0].phoneNumber").isString())
+                .andExpect(jsonPath("[0].address").isString())
+                .andExpect(jsonPath("[0].rank").isNumber())
+                .andExpect(jsonPath("[0].status").isString())
                 .andDo(document(DOCUMENT_NAME_PARTS_WINNERS, resourceSnippetAuthed("파츠 당첨자 명단 조회")));
 
     }
@@ -125,14 +113,12 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("추첨 당첨자를 확인처리 한다.")
     void lotteryWinnerCheckDone() throws Exception {
 
-        //when & then
-        this.mockMvc.perform(post(PATH_ADMIN_LOTTERY_WINNER_CHECK, TEST_UID)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
-                ).andExpect(status().isOk())
+        whenLotteryWinnerCheck();
+
+        resultActions
+                .andExpect(status().isOk())
                 .andDo(document(DOCUMENT_NAME_ADMIN_LOTTERY_WINNER_CHECK,
                         resourceSnippetAuthed("추첨 당첨자 확인처리")));
-
-        verify(lotteryWinnerService).lotteryWinnerCheck(TEST_UID);
 
     }
 
@@ -140,14 +126,12 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("파츠 추첨 당첨자를 확인처리 한다.")
     void partsWinnerCheckDone() throws Exception {
 
-        //when & then
-        this.mockMvc.perform(post(PATH_ADMIN_PARTS_WINNER_CHECK, TEST_UID)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
-                ).andExpect(status().isOk())
+        whenPartsWinnerCheck();
+
+        resultActions
+                .andExpect(status().isOk())
                 .andDo(document(DOCUMENT_NAME_ADMIN_PARTS_WINNER_CHECK,
                         resourceSnippetAuthed("파츠 추첨 당첨자 확인처리")));
-
-        verify(partsService).partsWinnerCheck(TEST_UID);
 
     }
 
@@ -155,13 +139,12 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("추첨 이벤트 응모자에 대해 추첨을 진행한다.")
     void lottery() throws Exception {
 
-        this.mockMvc.perform(post(PATH_LOTTERY)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN))
+        whenLottery();
+
+        resultActions
                 .andExpect(status().isOk())
                 .andDo(document(DOCUMENT_NAME_LOTTERY,
                         resourceSnippetAuthed("추첨 이벤트 응모자에 대해 추첨")));
-
-        verify(lotteryService).lottery();
 
     }
 
@@ -169,14 +152,89 @@ class AdminLotteryControllerTest extends ControllerTest {
     @DisplayName("파츠 이벤트 응모자에 대해 추첨을 진행한다.")
     void partsLottery() throws Exception {
 
-        this.mockMvc.perform(post(PATH_PARTS_LOTTERY)
-                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN))
+        whenPartsLottery();
+
+        resultActions
                 .andExpect(status().isOk())
                 .andDo(document(DOCUMENT_NAME_PARTS_LOTTERY,
                         resourceSnippetAuthed("파츠 이벤트 응모자에 대해 추첨")));
 
-        verify(partsService).partsLottery();
+    }
 
+    private void givenLotteryApplierList() {
+        Pageable pageable = PageRequest.of(TEST_PAGE_NUMBER, TEST_PAGE_SIZE);
+
+        Mockito.when(lotteryService.getApplierInfoPage(pageable))
+                .thenReturn(new PageImpl<>(
+                        List.of(
+                                ResponseLotteryApplierDto.createTestDto(),
+                                ResponseLotteryApplierDto.createTestDto()
+                        ), pageable, TEST_PAGE_SIZE));
+    }
+
+    private void givenLotteryWinners() {
+
+        Mockito.when(lotteryWinnerService.getAdminLotteryWinners())
+                .thenReturn(List.of(
+                                ResponseAdminLotteryWinnerDto.createTestDto(),
+                                ResponseAdminLotteryWinnerDto.createTestDto()
+                        )
+                );
+    }
+
+    private void givenPartsWinnerList(){
+        Mockito.when(partsService.getAdminPartsWinners())
+                .thenReturn(List.of(
+                        ResponseAdminPartsWinnerDto.createTestDto(),
+                        ResponseAdminPartsWinnerDto.createTestDto()
+                ));
+    }
+
+    private void whenLotteryApplierListAreRetrievedForAdmin() throws Exception {
+        resultActions = mockMvc.perform(get(PATH_ADMIN_APPLIER)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
+                .param(PARAM_PAGE, String.valueOf(TEST_PAGE_NUMBER))
+                .param(PARAM_SIZE, String.valueOf(TEST_PAGE_SIZE))
+        );
+    }
+
+    private void whenLotteryWinnerListAreRetrievedForAdmin() throws Exception {
+        resultActions = mockMvc.perform(get(PATH_ADMIN_LOTTERY_WINNERS)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
+        );
+    }
+
+    private void whenPartsWinnerListAreRetrievedForAdmin() throws Exception {
+        resultActions = mockMvc.perform(get(PATH_ADMIN_PARTS_WINNERS)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN));
+    }
+
+    private void whenLotteryWinnerCheck() throws Exception {
+        resultActions = mockMvc.perform(post(PATH_ADMIN_LOTTERY_WINNER_CHECK, TEST_UID)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
+        );
+        verify(lotteryWinnerService).lotteryWinnerCheck(TEST_UID);
+    }
+
+    private void whenPartsWinnerCheck() throws Exception {
+        resultActions = mockMvc.perform(post(PATH_ADMIN_PARTS_WINNER_CHECK, TEST_UID)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN)
+        );
+        verify(partsService).partsWinnerCheck(TEST_UID);
+    }
+
+    private void whenLottery() throws Exception {
+        resultActions = mockMvc.perform(post(PATH_LOTTERY)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN));
+
+        verify(lotteryService).lottery();
+    }
+
+    private void whenPartsLottery() throws Exception {
+        resultActions = mockMvc.perform(post(PATH_PARTS_LOTTERY)
+                .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN));
+
+        verify(partsService).partsLottery();
     }
 
 }
