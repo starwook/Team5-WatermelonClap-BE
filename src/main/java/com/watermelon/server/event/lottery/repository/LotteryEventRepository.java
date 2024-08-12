877cdef405bfa6e98d0c183ba8a108905f095959
ysndy <ysndy@naver.com>
2024-08-12 16:52:37 +0900

    fix[api-server]: 추첨 이벤트 레포지토리 수정

--- src/main/java/com/watermelon/server/event/lottery/repository/LotteryEventRepository.java
+++ src/main/java/com/watermelon/server/event/lottery/repository/LotteryEventRepository.java
@@ -3,5 +3,5 @@ package com.watermelon.server.event.lottery.repository;
 import com.watermelon.server.event.lottery.domain.LotteryEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LotteryEventRepository extends JpaRepository<Long, LotteryEvent> {
+public interface LotteryEventRepository extends JpaRepository<LotteryEvent, Long> {
 }
