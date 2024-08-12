2bfa7c7d7fff5ade57c6710df5525b055aebebf3
ysndy <ysndy@naver.com>
2024-08-12 16:52:28 +0900

    feat[api-server]: 추첨 이벤트 엔티티 수정

--- src/main/java/com/watermelon/server/event/lottery/domain/LotteryEvent.java
+++ src/main/java/com/watermelon/server/event/lottery/domain/LotteryEvent.java
@@ -2,16 +2,21 @@ package com.watermelon.server.event.lottery.domain;
 
 import com.watermelon.server.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
 public class LotteryEvent extends BaseEntity {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
 
     private String name;
@@ -20,7 +25,17 @@ public class LotteryEvent extends BaseEntity {
 
     private LocalDateTime endTime;
 
+    @Cascade(CascadeType.ALL)
     @OneToMany(mappedBy = "lotteryEvent")
     private List<LotteryReward> rewards;
+
+    public static LotteryEvent create(String name, LocalDateTime startTime, LocalDateTime endTime, List<LotteryReward> rewards) {
+        LotteryEvent event = new LotteryEvent();
+        event.name = name;
+        event.startTime = startTime;
+        event.endTime = endTime;
+        event.rewards = rewards;
+        return event;
+    }
 
 }
