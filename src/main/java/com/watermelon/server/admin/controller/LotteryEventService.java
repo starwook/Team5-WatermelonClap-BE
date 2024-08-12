09ab32dfcce4972d6261218c33e03f549dfa5e70
ysndy <ysndy@naver.com>
2024-08-12 16:52:46 +0900

    fix[api-server]: 추첨 이벤트 서비스 주석 추가

--- src/main/java/com/watermelon/server/admin/controller/LotteryEventService.java
+++ src/main/java/com/watermelon/server/admin/controller/LotteryEventService.java
@@ -1,5 +1,6 @@
 package com.watermelon.server.admin.controller;
 
+import com.watermelon.server.admin.exception.S3ImageFormatException;
 import com.watermelon.server.event.lottery.dto.request.RequestLotteryEventDto;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,6 +8,12 @@ import java.util.List;
 
 public interface LotteryEventService {
 
-    void createLotteryEvent(RequestLotteryEventDto requestLotteryEventDto, List<MultipartFile> images);
+    /**
+     * 이미지와 정보를 받아서 S3에 업로드하고 저장한다.
+     * @param requestLotteryEventDto 이미지 외 정보
+     * @param images 이미지
+     * @throws S3ImageFormatException
+     */
+    void createLotteryEvent(RequestLotteryEventDto requestLotteryEventDto, List<MultipartFile> images) throws S3ImageFormatException;
 
 }
