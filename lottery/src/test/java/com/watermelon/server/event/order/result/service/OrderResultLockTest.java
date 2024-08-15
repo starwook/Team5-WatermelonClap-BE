29c697056002d084bfc2f100fd5d4821510d8667
starwook <star.wookk@gmail.com>
2024-08-15 23:17:36 +0900

    feat[api-server]: 선착순 이벤트 퀴즈 제출시 boolean 검사 추가

--- lottery/src/test/java/com/watermelon/server/event/order/result/service/OrderResultLockTest.java
+++ lottery/src/test/java/com/watermelon/server/event/order/result/service/OrderResultLockTest.java
@@ -31,7 +31,7 @@ class OrderResultLockTest {
 
     @Test
     void 선착순_이벤트_락_적용_100명() throws InterruptedException {
-        int numberOfThreads = currentOrderEventManageService.getMaxWinnerCount()*5;
+        int numberOfThreads = currentOrderEventManageService.getMaxWinnerCount()*10;
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
         CountDownLatch latch = new CountDownLatch(numberOfThreads);
         for(int i=0;i<numberOfThreads;i++){
