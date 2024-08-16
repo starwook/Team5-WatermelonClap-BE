6fcf69f1f91a7e0f40626b542f4beb4bf03559e4
ysndy <ysndy@naver.com>
2024-08-16 18:00:11 +0900

    refactor[api-server]: 링크 저장 방식을 쿠키로 수정

--- lottery/src/main/java/com/watermelon/server/event/parts/controller/PartsController.java
+++ lottery/src/main/java/com/watermelon/server/event/parts/controller/PartsController.java
@@ -7,6 +7,8 @@ import com.watermelon.server.event.parts.dto.response.ResponsePartsDrawDto;
 import com.watermelon.server.event.parts.dto.response.ResponseRemainChanceDto;
 import com.watermelon.server.event.parts.exception.PartsDrawLimitExceededException;
 import com.watermelon.server.event.parts.service.PartsService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;  
 
 import java.util.List;
+
+import static com.watermelon.server.common.constants.HttpConstants.HEADER_LINK_ID;
 
 @RestController
 @RequiredArgsConstructor
@@ -57,8 +61,10 @@ public class PartsController {
 
     @GetMapping("/link/{link_key}")
     public ResponseEntity<List<ResponseMyPartsListDto>> getLinkPartsList(
-            @PathVariable String link_key
+            @PathVariable String link_key,
+            HttpServletResponse response
     ){
+        makeLinkCookie(response, link_key);
         return new ResponseEntity<>(partsService.getPartsList(link_key), HttpStatus.OK);
     }
     @ExceptionHandler(PartsDrawLimitExceededException.class)
@@ -66,5 +72,18 @@ public class PartsController {
         return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(ErrorResponse.of(partsDrawLimitExceededException.getMessage()));
     }
 
+    private void makeLinkCookie(HttpServletResponse response, String link_key){
+        // 쿠키 생성
+        Cookie cookie = new Cookie(HEADER_LINK_ID, link_key);
+
+        // 쿠키 설정
+        cookie.setHttpOnly(true); // 클라이언트 측 JavaScript에서 접근 불가
+        //cookie.setSecure(true);   // HTTPS에서만 전송
+        cookie.setMaxAge(7 * 24 * 60 * 60); // 쿠키의 유효 기간 (7일)
+        cookie.setPath("/");      // 모든 경로에서 유효
+        //cookie.setDomain("example.com"); // 쿠키를 설정할 도메인
+
+        response.addCookie(cookie);
+    }
 
 }
