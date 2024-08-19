8c782063cb1c6ac5c4fba4298696a1d1852a24e9
조도연 <ysndy1234@gmail.com>
2024-08-19 17:59:31 +0900

    refactor[api-server]: CORS 설정 제거 (#99)

--- lottery/src/main/java/com/watermelon/server/common/filter/CorsFilter.java
+++ lottery/src/main/java/com/watermelon/server/common/filter/CorsFilter.java
@@ -12,46 +12,46 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-
-        List<String> allowedOrigins = Arrays.asList("http://localhost:5173", "http://localhost:63342", "https://watermelon-clap.web.app");
-        String origin = request.getHeader("Origin");
-
-        if (allowedOrigins.contains(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", origin);
-        }
-
-
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization, ApplyTicket");
-
-        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        }else {
-            chain.doFilter(req, res);
-        }
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CorsFilter implements Filter {
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        HttpServletResponse response = (HttpServletResponse) res;
+//
+//        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//
+//        List<String> allowedOrigins = Arrays.asList("http://localhost:5173", "http://localhost:63342", "https://watermelon-clap.web.app");
+//        String origin = request.getHeader("Origin");
+//
+//        if (allowedOrigins.contains(origin)) {
+//            response.setHeader("Access-Control-Allow-Origin", origin);
+//        }
+//
+//
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers",
+//                "Origin, X-Requested-With, Content-Type, Accept, Authorization, ApplyTicket");
+//
+//        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        }else {
+//            chain.doFilter(req, res);
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
