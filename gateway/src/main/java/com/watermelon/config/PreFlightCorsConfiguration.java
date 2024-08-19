1df9aa63738b3fc592468a7cd153d46859343300
starwook <star.wookk@gmail.com>
2024-08-19 15:58:55 +0900

    refactor: cors 허용 설정을 WebFluxConfigurer클래스를 Override하여 구현

--- gateway/src/main/java/com/watermelon/config/PreFlightCorsConfiguration.java
+++ gateway/src/main/java/com/watermelon/config/PreFlightCorsConfiguration.java
@@ -8,13 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Configuration
-public class PreFlightCorsConfiguration {
+@EnableWebFlux
+public class PreFlightCorsConfiguration implements WebFluxConfigurer {
     private static final String ALLOWED_HEADERS = "Origin, X-Requested-With, Content-Type, Accept, Authorization, ApplyTicket";
 
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
@@ -25,37 +29,47 @@ public class PreFlightCorsConfiguration {
 
     private static final String MAX_AGE = "3600";
 
-    @Bean
-    public WebFilter corsFilter() {
-
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (CorsUtils.isPreFlightRequest(request)) {
-                ServerHttpResponse response = ctx.getResponse();
-
-                HttpHeaders headers = response.getHeaders();
-
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-
-                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-
-                headers.add("Access-Control-Max-Age", MAX_AGE);
-
-                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
-
-                headers.add("Access-Control-Allow-Credentials",ALLOWED_CREDENTIALS);
-
-                if (request.getMethod() == HttpMethod.OPTIONS) {
-
-                    response.setStatusCode(HttpStatus.OK);
-
-                    return Mono.empty();
-                }
-            }
-            return chain.filter(ctx);
-
-        };
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGIN)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedHeaders(ALLOWED_HEADERS)
+                .allowCredentials(true)
+                .maxAge(Long.parseLong(MAX_AGE))
+                ;
     }
+//    @Bean
+//    public WebFilter corsFilter() {
+//
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (CorsUtils.isPreFlightRequest(request)) {
+//                ServerHttpResponse response = ctx.getResponse();
+//
+//                HttpHeaders headers = response.getHeaders();
+//
+//                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+//
+//                headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+//
+//                headers.add("Access-Control-Max-Age", MAX_AGE);
+//
+//                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
+//
+//                headers.add("Access-Control-Allow-Credentials",ALLOWED_CREDENTIALS);
+//
+//                if (request.getMethod() == HttpMethod.OPTIONS) {
+//
+//                    response.setStatusCode(HttpStatus.OK);
+//
+//                    return Mono.empty();
+//                }
+//            }
+//            return chain.filter(ctx);
+//
+//        };
+//
+//    }
 
 }
