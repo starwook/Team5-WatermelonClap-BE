51a9c021804091723e8c2aa7bc45371770c2e263
ysndy <ysndy@naver.com>
2024-08-20 18:10:58 +0900

    fix[api-server]: 링크 아이디 검증 방식 쿠키로 변경

--- lottery/src/main/java/com/watermelon/server/event/lottery/interceptor/FirstLoginInterceptor.java
+++ lottery/src/main/java/com/watermelon/server/event/lottery/interceptor/FirstLoginInterceptor.java
@@ -2,16 +2,21 @@ package com.watermelon.server.event.lottery.interceptor;
 
 import com.watermelon.server.event.link.service.LinkService;
 import com.watermelon.server.event.lottery.service.LotteryService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Arrays;
 
 import static com.watermelon.server.common.constants.HttpConstants.*;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FirstLoginInterceptor implements HandlerInterceptor {
 
     private final LotteryService lotteryService;
@@ -20,9 +25,23 @@ public class FirstLoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
+        String linkId = null;
+        Cookie[] cookies = request.getCookies();
+
+        log.info("cookies: {}", Arrays.toString(cookies));
+
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                String cookieName = cookie.getName();
+                if(cookieName.equals(HEADER_LINK_ID)) linkId = cookie.getValue();
+            }
+        }
+
+        log.info("linkId:{}", linkId);
+
         checkFirstLogin(
                 request.getAttribute(HEADER_UID).toString(),
-                request.getHeader(HEADER_LINK_ID)
+                linkId
         );
 
         return true;
