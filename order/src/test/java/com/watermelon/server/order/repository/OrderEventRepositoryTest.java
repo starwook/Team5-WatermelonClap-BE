08e06d2691a3e5ae996e462da4555205d25ca272
ysndy <ysndy@naver.com>
2024-08-22 23:40:24 +0900

    refactor[api-server]: 선착순 레포지토리 테스트 설명 통일된 양식으로 수정

--- order/src/test/java/com/watermelon/server/order/repository/OrderEventRepositoryTest.java
+++ order/src/test/java/com/watermelon/server/order/repository/OrderEventRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.TestPropertySource;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) //replace None 사용시 실제 DB사용
 @TestPropertySource("classpath:application-local-db.yml")
+@DisplayName("[단위] 선착순 레포지토리")
 public class OrderEventRepositoryTest
 {
 
