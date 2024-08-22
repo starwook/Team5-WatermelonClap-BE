1eedc83d670ba6ed9a6e25a3bedc538c8950620a
ysndy <ysndy@naver.com>
2024-08-22 23:06:32 +0900

    refactor[api-server]: 링크 서비스 테스트 설명 통일된 양식으로 수정

--- lottery/src/test/java/com/watermelon/server/event/link/utils/LinkUtilsTest.java
+++ lottery/src/test/java/com/watermelon/server/event/link/utils/LinkUtilsTest.java
@@ -1,15 +1,18 @@
 package com.watermelon.server.event.link.utils;
 
 import com.watermelon.server.event.link.utils.LinkUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("[단위] 링크 유틸 테스트")
 class LinkUtilsTest {
 
     @Test
+    @DisplayName("인코딩 & 디코딩 - 성공")
     void test(){
 
         //given
