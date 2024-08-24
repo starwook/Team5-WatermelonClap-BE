a482a4e69bbf7a235eb98c08fffc82da5012d2b0
starwook <star.wookk@gmail.com>
2024-08-24 15:04:09 +0900

    feat: OrderResultSaveService가 다른 DataSource를 주입받는지 확인하는 테스트 구현

---
+++ order/src/test/java/com/watermelon/server/order/service/HikariDataSourceTest.java
@@ -0,0 +1,51 @@
+package com.watermelon.server.order.service;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.TransactionManager;
+
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+@DisplayName("[통합] 커넥션 풀")
+class HikariDataSourceTest {
+
+    @Autowired
+    private OrderResultSaveService orderResultSaveService;
+    @Autowired
+    private HikariDataSource originalHikariDataSource;
+
+
+    @Test
+    @DisplayName("")
+    public void isCustomHikariDataSource() {
+        Assertions.assertThat(originalHikariDataSource.getConnectionTimeout()).isNotEqualTo(originalHikariDataSource.getDataSource());
+
+        /*ExecutorService executorService = Executors.newFixedThreadPool(maximumPoolSize);
+        CountDownLatch latch = new CountDownLatch(maximumPoolSize);
+        for(int i=0;i<maximumPoolSize;i++){//maximumpool만큼 잠든 후에
+            int finalI = i;
+            executorService.submit(()->{
+                try{
+
+                    orderResultSaveService.sleepWithTimeOut(timeOut*2); //두배만큼 쉬어준다
+                } finally {
+                    latch.countDown();
+
+                }
+            });
+        }
+        orderResultSaveService.isOrderApplyNotFullThenSaveConnectionOpen("applyToken");*/
+    }
+}
