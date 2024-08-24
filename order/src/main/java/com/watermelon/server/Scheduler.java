14fef3639c9595b0142669eda7fdb437e0e8b3f4
starwook <star.wookk@gmail.com>
2024-08-25 02:56:34 +0900

    refactor: 선착순 이벤트 스케쥴링 주기 30초 -> 1초로 수정
    [=]

--- order/src/main/java/com/watermelon/server/Scheduler.java
+++ order/src/main/java/com/watermelon/server/Scheduler.java
@@ -16,7 +16,7 @@ public class Scheduler {
 
     private final OrderEventSchedulingService orderEventSchedulingService;
     private final CurrentOrderEventManageService currentOrderEventManageService;
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRate = 1000)
     public void checkOrderEvent(){
         Long currentEventId = currentOrderEventManageService.getCurrentOrderEventId();
         orderEventSchedulingService.changeOrderStatusByTime();
