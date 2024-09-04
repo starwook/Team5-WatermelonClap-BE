e5bfb0ecf72f5ad037813c067c2a3295f487cf06
유성욱 <88507708+starwook@users.noreply.github.com>
2024-09-04 21:13:19 +0900

    refactor: 테스트 기준 yml 파일 local로 수정 (#2)
    * refactor: 테스트 기준 yml 파일 local로 수정
    
    * refactor: DatasourceConfig 의존하는 yml 파일 설정 수정
    
    * refactor: Pr시 트리거가 작동되도록 수정
    
    * build: ncp CD작업 추가
    
    * docs: appleboy version 수정
    
    * build: deploy.yml에서 주석 부분을 제거
    
    * build: build와 deploy 부분을 분리
    
    * build: appleboy 1.0.3 버전 수정
    
    * build: cloud agent 주석 적용
    
    * build: host 줄 분리
    
    * build: script문 줄 맞춤 수정
    
    * build: with문 줄 맞춤 수정
    
    * build: 테스틀흘 위한 scrpit문 주석 적용
    
    * build: deploy steps 합치며 주석 들여쓰기 수정
    
    * feat: NCP 용 Pinpoint agent 파일 추가
    
    * refactor: 모니터링 서버 IP 수정
    
    * refactor: Connection을 기다리는 시간을 15초로 수정

--- order/src/main/java/com/watermelon/server/orderResult/service/OrderResultCommandService.java
+++ order/src/main/java/com/watermelon/server/orderResult/service/OrderResultCommandService.java
@@ -36,21 +36,22 @@ public class OrderResultCommandService {
 
     public ResponseApplyTicketDto createTokenAndMakeTicket(Long orderEventId) {
         String applyToken = applyTokenProvider.createTokenByOrderEventId(JwtPayload.from(String.valueOf(orderEventId)));
-        for(int i=0;i<toGetConnectionCount;i++) {
-            try{
-                if(orderResultSaveService.isOrderApplyNotFullThenSaveConnectionOpen(applyToken)){ // 커넥션이 열리는 메소드
-                    return ResponseApplyTicketDto.applySuccess(applyToken);
-                }
-                return ResponseApplyTicketDto.fullApply();
-            }
-            catch (CannotCreateTransactionException e){
-                e.printStackTrace();
-                if(currentOrderEventManageService.isOrderApplyFull()){
-                    return ResponseApplyTicketDto.fullApply();
-                }
-            }
+        if(orderResultSaveService.isOrderApplyNotFullThenSaveConnectionOpen(applyToken)){ // 커넥션이 열리는 메소드
+            return ResponseApplyTicketDto.applySuccess(applyToken);
         }
         return ResponseApplyTicketDto.fullApply();
+//        for(int i=0;i<toGetConnectionCount;i++) {
+//            try{
+//
+//            }
+//            catch (CannotCreateTransactionException e){
+//                e.printStackTrace();
+//                if(currentOrderEventManageService.isOrderApplyFull()){
+//                    return ResponseApplyTicketDto.fullApply();
+//                }
+//            }
+//        }
+//        return ResponseApplyTicketDto.fullApply();
     }
 
 
